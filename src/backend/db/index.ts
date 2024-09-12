b1bb449d80babc30d9ab741ab0ec40b30396e37f
AllanJeremy <aj.dev254@gmail.com>
2024-09-13 01:18:56 +0300

    build: added a factory function for creating a database

--- src/backend/db/index.ts
+++ src/backend/db/index.ts
@@ -1,1 +1,8 @@
-// TODO: Add db export here
+import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
+import * as schema from './schema';
+
+export const createDb = (databaseUrl: string) => {
+	const sql = neon(databaseUrl);
+	return drizzle(sql, { schema });
+};
