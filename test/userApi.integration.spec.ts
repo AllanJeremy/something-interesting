5661f35b18758cdc0d5a2f2d89a25b899c43e2ed
AllanJeremy <aj.dev254@gmail.com>
2024-09-12 03:52:18 +0300

    build: added base test in integration test 2

--- test/userApi.integration.spec.ts
+++ test/userApi.integration.spec.ts
@@ -1,7 +1,9 @@
-import { describe, it, expect, beforeAll, afterAll } from 'vitest';
+import { describe, it, expect /* beforeAll, afterAll */ } from 'vitest';
 // import { unstable_dev, UnstableDevWorker } from 'wrangler';
 
 describe('Hello World worker', () => {
 	// TODO: Add actual tests
-	expect(true).toBe(true);
+	it('runs correctly', () => {
+		expect(true).toBe(true);
+	});
 });
