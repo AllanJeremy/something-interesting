f592d9edfa25e9b5702818f7c876b6c524525f2f
AllanJeremy <aj.dev254@gmail.com>
2024-09-13 02:09:02 +0300

    build using API response generator for route handler

--- src/backend/routes/users.routes.ts
+++ src/backend/routes/users.routes.ts
@@ -1,28 +1,40 @@
 import { Hono } from 'hono';
 import { Env, Vars } from '../backend.routes';
+import { generateApiErrorResponse, generateApiSuccessResponse } from '../utils/api.utils';
+import { CreateUserData, User } from '../types';
 
 const app = new Hono<{ Bindings: Env; Variables: Vars }>();
 
 //#region Users
 // Create a new user.
+//TODO: Add validation for the request body using Zod
 app.post('/', async (c) => {
-	const userService = c.get('userService');
+	try {
+		const userService = c.get('userService');
 
-	const userCreated = await userService.createUser({
-		username: 'Allan',
-		email: 'aj.dev254@gmail.com',
-	});
+		const body = await c.req.json();
+		const createUserData: CreateUserData = {
+			username: body.username,
+			email: body.email,
+		};
 
-	console.log('userCreated: ', userCreated);
+		const userCreated: User = await userService.createUser(createUserData);
 
-	return c.json({ message: 'User created', data: userCreated });
+		console.log('userCreated: ', userCreated);
+
+		return c.json(generateApiSuccessResponse<User>(userCreated, 'User created'));
+	} catch (error: unknown) {
+		return c.json(generateApiErrorResponse(error, 'User creation failed'));
+	}
 });
 
 // Retrieve a list of all users.
 app.get('/', async (c) => {
 	const userService = c.get('userService');
 
-	return c.json({ message: 'listing all users' });
+	const usersFound: User[] = await userService.getAllUsers();
+
+	return c.json({ message: 'listing all users', data: usersFound });
 });
 
 //#endregion Users
