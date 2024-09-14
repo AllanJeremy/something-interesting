4848080ca61e5c78a4a966a73b4821e22c65e4f5
AllanJeremy <aj.dev254@gmail.com>
2024-09-14 16:20:08 +0300

    refactor: added optional cause in error utils

--- src/backend/utils/error.utils.ts
+++ src/backend/utils/error.utils.ts
@@ -1,43 +1,47 @@
-import { HTTPException } from 'hono/http-exception';
+// This file contains custom error classes that extend the base Error class
+// We are not using HTTPException because we want to have more control over the error responses & not to be locked to Hono (though this is inspired by Hono's HTTPException structure)
 import { StatusCode } from 'hono/utils/http-status';
 
-export class ApiError extends HTTPException {
-	constructor(message: string, public status: StatusCode = 500) {
-		super(status, { message });
+// Added here so we can always replace this with a different type if we want to (eg. if we are not using Hono anymore)
+type HttpStatusCode = StatusCode;
+
+export class ApiError extends Error {
+	constructor(message: string, public cause?: string, public status: HttpStatusCode = 500) {
+		super(message);
 	}
 }
 
 export class BadRequestError extends ApiError {
-	constructor(message: string) {
-		super(`Bad Request: ${message}`, 400);
+	constructor(message: string, cause?: string) {
+		super(`Bad Request: ${message}`, cause, 400);
 	}
 }
 
 export class UnauthorizedError extends ApiError {
-	constructor(message: string) {
-		super(`Unauthorized: ${message}`, 401);
+	constructor(message: string, cause?: string) {
+		super(`Unauthorized: ${message}`, cause, 401);
 	}
 }
 
 export class ForbiddenError extends ApiError {
-	constructor(message: string) {
-		super(`Forbidden: ${message}`, 403);
+	constructor(message: string, cause?: string) {
+		super(`Forbidden: ${message}`, cause, 403);
 	}
 }
 export class NotFoundError extends ApiError {
-	constructor(message: string) {
-		super(`Not Found: ${message}`, 404);
+	constructor(message: string, cause?: string) {
+		super(`Not Found: ${message}`, cause, 404);
 	}
 }
 
 export class ConflictError extends ApiError {
-	constructor(message: string) {
-		super(`Conflict: ${message}`, 409);
+	constructor(message: string, cause?: string) {
+		super(`Conflict: ${message}`, cause, 409);
 	}
 }
 
 export class InternalServerError extends ApiError {
-	constructor(message: string) {
-		super(`Internal Server Error: ${message}`, 500);
+	constructor(message: string, cause?: string) {
+		super(`Internal Server Error: ${message}`, cause, 500);
 	}
 }
