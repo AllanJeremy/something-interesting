1a5de69636ce92a159cc90fc2166219bd5a2e06b
AllanJeremy <aj.dev254@gmail.com>
2024-09-17 20:41:26 +0300

    fix: remove failing vite

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	resolve: {
-		alias: {
-			'@backend': path.resolve(__dirname, '../backend'),
-		},
-	},
+	resolve: {},
 	server: {
 		proxy: {
 			'/api': {
