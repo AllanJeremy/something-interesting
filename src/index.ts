226d0308f61003a7e9578089b4a338b896a6dd79
AllanJeremy <aj.dev254@gmail.com>
2024-09-17 20:05:16 +0300

    fix: fixed cors issues

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
 import { Hono } from 'hono';
 import backendRoutes from './backend/backend.routes';
+import { cors } from 'hono/cors';
 
 const app = new Hono<{ Bindings: Env }>();
+
+app.use(
+	'*',
+	// TODO: Get live URL from env
+	cors({
+		origin: ['http://localhost:5173', 'https://your-production-frontend-url.com'],
+		allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+		allowHeaders: ['Content-Type', 'Authorization'],
+		exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
+		maxAge: 600,
+		credentials: true,
+	})
+);
 
 app.route('/api', backendRoutes);
 
