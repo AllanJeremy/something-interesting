ed9b2bb187f884196f0d6aa09b409f1f458f3640
AllanJeremy <aj.dev254@gmail.com>
2024-09-17 18:32:11 +0300

    build: remove frontend routes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import { Hono } from 'hono';
 import backendRoutes from './backend/backend.routes';
-import frontendRoutes from './frontend/frontend.routes';
 
 const app = new Hono<{ Bindings: Env }>();
-
-app.route('/', frontendRoutes);
 
 app.route('/api', backendRoutes);
 
