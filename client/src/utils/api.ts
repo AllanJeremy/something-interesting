88c1b5750f6e0dfcac3aff8cbc342425de94d8b6
AllanJeremy <aj.dev254@gmail.com>
2024-09-19 01:06:44 +0300

    build: implemented CRUD functions in API util

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -2,13 +2,30 @@ import { ApiResponse } from "@server/types";
 
 const API_BASE_URL = "https://api.aj-doge.workers.dev";
 
-export async function fetchApi<T>(
+async function _fetchApi<T>(
 	path: string,
 	init?: RequestInit
 ): Promise<ApiResponse<T>> {
-	return {
-		ok: true,
-		message: "",
-		data: {} as T,
-	};
+	const url = `${API_BASE_URL}/${path}`;
+
+	const response = await fetch(url, init);
+	const apiResponse = (await response.json()) as ApiResponse<T>;
+
+	return apiResponse;
+}
+
+export function getRequest<T = unknown>(path: string, init?: RequestInit) {
+	return _fetchApi<T>(path, { ...init, method: "GET" });
+}
+
+export function postRequest<T = unknown>(path: string, init?: RequestInit) {
+	return _fetchApi<T>(path, { ...init, method: "POST" });
+}
+
+export function patchRequest<T = unknown>(path: string, init?: RequestInit) {
+	return _fetchApi<T>(path, { ...init, method: "PATCH" });
+}
+
+export function deleteRequest<T = unknown>(path: string, init?: RequestInit) {
+	return _fetchApi<T>(path, { ...init, method: "PATCH" });
 }
