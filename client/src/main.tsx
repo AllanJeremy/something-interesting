a6a4bc0e98ed528ee9f888d566258761f9173cd8
AllanJeremy <aj.dev254@gmail.com>
2024-09-19 22:52:03 +0300

    build: added sonner toast

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "./components/ui/sonner.tsx";
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<QueryClientProvider client={queryClient}>
 			<App />
+			<Toaster />
 			<ReactQueryDevtools />
 		</QueryClientProvider>
 	</StrictMode>
