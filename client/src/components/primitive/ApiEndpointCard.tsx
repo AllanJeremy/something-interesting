4b9560ea1713b0a9aea381b4c81f4300a9260030
AllanJeremy <aj.dev254@gmail.com>
2024-09-19 22:54:34 +0300

    build: improve ApiEndpointCard component

--- client/src/components/primitive/ApiEndpointCard.tsx
+++ client/src/components/primitive/ApiEndpointCard.tsx
@@ -1,4 +1,7 @@
 import { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert";
+import { Button } from "../ui/button";
+import { Copy } from "lucide-react";
+import { toast } from "sonner";
 
 const apiUrl = import.meta.env.VITE_API_BASE_URL;
 
@@ -9,6 +12,15 @@ type ApiEndpointCardProps = {
 	method: string;
 	endpoint: string;
 };
+
+function _copyApiEndpoint(title: string, path: string) {
+	const fullApiUrl = `${apiUrl}${path}`;
+
+	navigator.clipboard.writeText(fullApiUrl);
+	toast.success(`Copied "${title}" endpoint URL: ${fullApiUrl}`, {
+		position: "bottom-center",
+	});
+}
 
 function ApiEndpointCard({
 	className,
@@ -22,13 +34,20 @@ function ApiEndpointCard({
 			<AlertTitle>{title}</AlertTitle>
 			{description && <AlertDescription>{description}</AlertDescription>}
 			<AlertDescription className="pt-sm">
-				<div className="relative mt-2 p-2 bg-gray-100 rounded-md border border-gray-300">
-					<pre className="m-0">
-						<code className="text-blue-600">
+				<div className="relative mt-2 p-2 bg-gray-100 rounded-md border border-gray-300 overflow-auto">
+					<div className="flex justify-between items-center">
+						<code className="text-blue-600 text-xs break-all w-4/5">
 							{method} {apiUrl}
 							{endpoint}
 						</code>
-					</pre>
+						<Button
+							className="ml-2 "
+							variant={"outline"}
+							onClick={() => _copyApiEndpoint(title, endpoint)}
+						>
+							<Copy className="h-4 w-4" />
+						</Button>
+					</div>
 				</div>
 			</AlertDescription>
 		</Alert>
