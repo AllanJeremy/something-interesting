a6267415e163604bde8f99704b59748fc3737e08
AllanJeremy <aj.dev254@gmail.com>
2024-09-20 02:10:05 +0300

    build: add placeholder for users table

--- client/src/components/users/_partials/UsersTable.tsx
+++ client/src/components/users/_partials/UsersTable.tsx
@@ -1,11 +1,30 @@
+import ErrorCard from "@/components/primitive/ErrorCard";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useUser } from "@/hooks";
+
 function UsersTable() {
+	const { userQuery } = useUser();
+
+	if (userQuery.isLoading) {
+		return <Skeleton />;
+	}
+
+	if (userQuery.isError || !userQuery.data) {
+		return <ErrorCard error={new Error("bruh")} />;
+	}
+
 	return (
 		<>
 			<section>
-				<h3>Users</h3>
-			</section>
-			<section>
-				<h3>Friends</h3>
+				<h1> User data</h1>
+				{userQuery.data.map((user) => (
+					<code key={user.id}>
+						<pre>
+							id: {user.id} | @{user.username} | {user.email} created on{" "}
+							{user.createdAt.toString()}
+						</pre>
+					</code>
+				))}
 			</section>
 		</>
 	);
