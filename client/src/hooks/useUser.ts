9e011e180388606e3d73da06444a9c8b1042a2aa
AllanJeremy <aj.dev254@gmail.com>
2024-09-20 02:09:21 +0300

    build: implement useUser hook with tanstack query

--- client/src/hooks/useUser.ts
+++ client/src/hooks/useUser.ts
@@ -1,31 +1,98 @@
-import { useState } from "react";
-import { type User } from "@server/types";
+import { getRequest, postRequest } from "@/utils/api";
+import { CreateUserData, User } from "@server/types";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { toast } from "sonner";
 
-export const useUser = () => {
-	//
-	const [users, _setUsers] = useState<User[]>([]);
-	const [usersAreLoading, _setUsersAreLoading] = useState(true);
-	const [activeUser, setActiveUser] = useState<User | null>(null);
+export const useUser = (
+	onCreateSuccess?: (userCreated: User) => void,
+	onCreateError?: (error: Error) => void
+) => {
+	//#region API requests
+	/** Makes the API request to create the user */
+	async function _createUser(createUserData: CreateUserData): Promise<User> {
+		const { data: userCreated } = await postRequest<User>(
+			"/users",
+			createUserData
+		);
 
-	function loadUsers() {
-		// TODO: add implementation
-		_setUsersAreLoading(false);
-		_setUsers([]);
+		return userCreated;
 	}
 
-	function resetActiveUser() {
-		setActiveUser(null);
+	/** Makes the API request to load all the users */
+	// TODO: Add search, pagination
+	async function _loadAllUsers(
+		search?: string,
+		page?: number,
+		limit?: number
+	): Promise<User[]> {
+		const urlSearchParams = new URLSearchParams();
+
+		if (search) {
+			urlSearchParams.append("search", search);
+		}
+
+		if (page) {
+			urlSearchParams.append("page", String(page));
+		}
+
+		if (limit) {
+			urlSearchParams.append("limit", String(limit));
+		}
+
+		let createUserApiUrl = "/users";
+
+		if (urlSearchParams.size > 0) {
+			createUserApiUrl += `?${urlSearchParams.toString()}`;
+		}
+
+		const { data: users } = await getRequest<User[]>(createUserApiUrl);
+
+		return users;
 	}
+	//#endregion
+
+	//#region Queries
+	const userQuery = useQuery({
+		queryKey: ["users"],
+		queryFn: () => _loadAllUsers(),
+	});
+
+	const createUserMutation = useMutation({
+		mutationFn: _createUser,
+		onError: (error) => {
+			toast.error(error.message);
+
+			if (onCreateError) {
+				onCreateError(error);
+			}
+		},
+		onSuccess: (userCreated) => {
+			toast.success(
+				`User @${userCreated.username} created (${userCreated.email})`
+			);
+
+			if (onCreateSuccess) {
+				onCreateSuccess(userCreated);
+			}
+		},
+	});
+
+	//#endregion Queries
+
+	//#region Exposed functions
+
+	/** Calls the create user mutation */
+	function handleCreateUser(createUserData: CreateUserData) {
+		createUserMutation.mutate(createUserData);
+	}
+	//#endregion Exposed functions
 
 	return {
-		// Functions
-		loadUsers,
-		setActiveUser,
-		resetActiveUser,
+		// React query related
+		userQuery,
+		createUserMutation,
 
-		// Variables
-		activeUser,
-		users,
-		usersAreLoading,
+		// Exposed functions
+		handleCreateUser,
 	};
 };
