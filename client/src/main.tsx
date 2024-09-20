22ec673613c42a1b58ed898229961580cde27cbf
AllanJeremy <aj.dev254@gmail.com>
2024-09-20 03:36:50 +0300

    build: set default stale time for queries to 1 minute

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,7 +6,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "./components/ui/sonner.tsx";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: 60 * 1000, // 60 seconds (1 minute)
+		},
+	},
+});
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
