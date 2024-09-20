4e62d79de335f4e20d0e3ebfec763333bfb6db4b
AllanJeremy <aj.dev254@gmail.com>
2024-09-20 03:20:43 +0300

    build: update table message in users container

--- client/src/components/users/UsersContainer.tsx
+++ client/src/components/users/UsersContainer.tsx
@@ -33,13 +33,10 @@ function UsersContainer() {
 						</Button>
 					</section>
 
-					<p className="mt-4 text-lg font-light text-slate-700/80">
-						Click on any user to view their friends. Toggle active user to
-						determine which user you want to send friend requests as.
-					</p>
 					<p className="mt-4 text-sm font-light text-slate-700/80">
-						In the real world, the active user would be the currently logged in
-						user
+						Currently, you can only create and view users. In the future, you
+						will be able to view friends for each user and toggle the active
+						user.
 					</p>
 				</CardHeader>
 				<CardContent>
