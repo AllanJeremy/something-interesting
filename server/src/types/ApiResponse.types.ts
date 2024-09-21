646489cef87564c8cea437d55c13447abba83254
AllanJeremy <aj.dev254@gmail.com>
2024-09-21 17:25:22 +0300

    refactor: use custom error type in error utils

--- server/src/types/ApiResponse.types.ts
+++ server/src/types/ApiResponse.types.ts
@@ -1,13 +1,15 @@
+import { ApiError } from '../utils/error.utils';
+
 export interface ApiSuccessResponse<T> {
 	readonly ok: true;
 	readonly data: T;
 	readonly message: string;
 }
 
-export interface ApiErrorResponse<E = unknown> {
+export interface ApiErrorResponse<E = ApiError> {
 	readonly ok: false;
 	readonly message: string;
-	readonly error?: E; // TODO: Consider using a custom error type
+	readonly error?: E;
 }
 
 export type ApiResponse<T, E = unknown> = ApiSuccessResponse<T> | ApiErrorResponse<E>;
