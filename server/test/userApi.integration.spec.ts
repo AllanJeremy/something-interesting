df35972551851e45e3ec7389b094f04e0d6b248f
AllanJeremy <aj.dev254@gmail.com>
2024-09-21 22:52:21 +0300

    debug: environment variables availability

--- server/test/userApi.integration.spec.ts
+++ server/test/userApi.integration.spec.ts
@@ -1,7 +1,9 @@
-import { SELF } from 'cloudflare:test';
+import { SELF, env } from 'cloudflare:test';
 import { describe, it, expect } from 'vitest';
 
 it('dispatches fetch event', async () => {
+	console.log('env:', env);
 	const response = await SELF.fetch('https://example.com');
+
 	expect(await response.text()).toBe('HEY!');
 });
