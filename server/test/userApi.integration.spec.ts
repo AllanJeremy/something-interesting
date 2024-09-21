41d98203085776fe48d9f29ed91a197f61e13137
AllanJeremy <aj.dev254@gmail.com>
2024-09-21 22:31:35 +0300

    build: test if integration tests run on server

--- server/test/userApi.integration.spec.ts
+++ server/test/userApi.integration.spec.ts
@@ -1,10 +1,21 @@
-import { describe, it, expect /* beforeAll, afterAll */ } from 'vitest';
-// import { unstable_dev, UnstableDevWorker } from 'wrangler';
+import { env, createExecutionContext, waitOnExecutionContext } from 'cloudflare:test';
+import { describe, it, expect } from 'vitest';
+// Could import any other source file/function here
+import worker from '../src';
 
-// TODO: Reference https://hono.dev/docs/getting-started/cloudflare-workers#testing
+// For now, you'll need to do something like this to get a correctly-typed
+// `Request` to pass to `worker.fetch()`.
+const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
+
 describe('Hello World worker', () => {
-	// TODO: Add actual tests
-	it('runs correctly', () => {
-		expect(true).toBe(true);
+	it('responds with Hello World!', async () => {
+		const request = new IncomingRequest('http://example.com/users');
+		// Create an empty context to pass to `worker.fetch()`
+		const ctx = createExecutionContext();
+		const response = await worker.fetch(request, env, ctx);
+		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
+		await waitOnExecutionContext(ctx);
+
+		expect(response.status).toBe('Hello World!');
 	});
 });
